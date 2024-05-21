04554e6ba19001b568ea59df1a0c4ab7a5e24c09
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-05-21 11:25:24 -0300

    Create Bet component

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,11 @@
 import './App.css';
-import { useState, useEffect } from 'react';
-import { generateBet } from './utils/generateBet';
-import { generateAvailableNumbersForBet } from './utils/generateAvailableNumbersForBet';
-import { sortNumbersAscending } from './utils/sortNumbersAscending';
+import { Bet } from './components/Bet';
 
 function App() {
-   const [generatedBet, setGeneratedBet] = useState<string[]>([]);
-   const [selectedNumbers, setSelectedNumbers] = useState<string[]>([]);
-
-   useEffect(() => {
-      setGeneratedBet(sortNumbersAscending(generateBet('15', '25')));
-   }, []);
-
-   function handleSelectedNumber(e: React.ChangeEvent<HTMLInputElement>) {
-      const selectedNumber = e.target.name;
-
-      if (e.target.checked) {
-         setSelectedNumbers((prevState) => [...prevState, selectedNumber]);
-      } else {
-         setSelectedNumbers((prevState) => {
-            return [
-               ...prevState.filter((numString) => numString !== selectedNumber),
-            ];
-         });
-      }
-   }
-
    return (
       <main>
          <h1>Checkbox com useState adicionando em array</h1>
-         <form id='bet-form'>
-            {generateAvailableNumbersForBet('25').map((numString) => (
-               <label key={numString}>
-                  <input
-                     type='checkbox'
-                     name={`${numString}`}
-                     id={`number-${numString}`}
-                     onChange={handleSelectedNumber}
-                  />
-                  {numString}
-               </label>
-            ))}
-         </form>
-
-         <h2>Números sorteados aleatoriamente crescente:</h2>
-         <div>
-            {generatedBet.map((numString) => (
-               <span key={numString}>{`${numString} `}</span>
-            ))}
-         </div>
-
-         <h2>Números na ordem escolhida:</h2>
-         <div>
-            {selectedNumbers.map((numString) => (
-               <span key={numString}>{`${numString} `}</span>
-            ))}
-         </div>
-
-         <h2>Números na ordem crescente:</h2>
-         <div>
-            {sortNumbersAscending(selectedNumbers).map((numString) => (
-               <span key={numString}>{`${numString} `}</span>
-            ))}
-         </div>
+         <Bet />
       </main>
    );
 }
---
+++ src/components/Bet.tsx
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import { generateBet } from '../utils/generateBet';
+import { generateAvailableNumbersForBet } from '../utils/generateAvailableNumbersForBet';
+import { sortNumbersAscending } from '../utils/sortNumbersAscending';
+
+export function Bet() {
+   const [generatedBet, setGeneratedBet] = useState<string[]>([]);
+   const [selectedNumbers, setSelectedNumbers] = useState<string[]>([]);
+
+   useEffect(() => {
+      setGeneratedBet(sortNumbersAscending(generateBet('15', '25')));
+   }, []);
+
+   function handleSelectedNumber(e: React.ChangeEvent<HTMLInputElement>) {
+      const selectedNumber = e.target.name;
+
+      if (e.target.checked) {
+         setSelectedNumbers((prevState) => [...prevState, selectedNumber]);
+      } else {
+         setSelectedNumbers((prevState) => {
+            return [
+               ...prevState.filter((numString) => numString !== selectedNumber),
+            ];
+         });
+      }
+   }
+
+   return (
+      <section>
+         <form id='bet-form'>
+            {generateAvailableNumbersForBet('25').map((numString) => (
+               <label key={numString}>
+                  <input
+                     type='checkbox'
+                     name={`${numString}`}
+                     id={`number-${numString}`}
+                     onChange={handleSelectedNumber}
+                  />
+                  {numString}
+               </label>
+            ))}
+         </form>
+
+         <h2>Números sorteados aleatoriamente crescente:</h2>
+         <div>
+            {generatedBet.map((numString) => (
+               <span key={numString}>{`${numString} `}</span>
+            ))}
+         </div>
+
+         <h2>Números na ordem escolhida:</h2>
+         <div>
+            {selectedNumbers.map((numString) => (
+               <span key={numString}>{`${numString} `}</span>
+            ))}
+         </div>
+
+         <h2>Números na ordem crescente:</h2>
+         <div>
+            {sortNumbersAscending(selectedNumbers).map((numString) => (
+               <span key={numString}>{`${numString} `}</span>
+            ))}
+         </div>
+      </section>
+   );
+}
