adc5e167d41cfdd0085c1f5c67c3a4fd58559536
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-05-31 15:49:10 -0300

    Add JSDoc in countHalfBet function

--- src/utils/countHalfBet.ts
+++ src/utils/countHalfBet.ts
@@ -1,4 +1,15 @@
-// Avaliar o uso para Lotomania
+/**
+ * @description Faz a contagem dos números que vêm antes e depois da metade dos números da aposta.
+ * @param {string} totalNumbersAvailable String -> Total de números disponíveis para a aposta.
+ * @param {string[]} selectedNumbers Array de string -> Array com os números selecionados para a aposta.
+ * @return {string} String -> Texto contendo a quantidade de números de antes e depois do meio da aposta.
+ * @example
+ * // Suponha que o total de números disponíveis são 80 e
+ * // os números selecionados da aposta forem: 01, 25, 56, 66, 79.
+ * console.log(countHalfBet("80", ["01", "25", "56", "66", "79"]));
+ * // Resultado esperado: 2 antes e 3 depois (metade até 40)
+ */
+
 export function countHalfBet(
    totalNumbersAvailable: string,
    selectedNumbers: string[]
