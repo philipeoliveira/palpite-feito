8184d54c960afd37836782c863337fe9b198492b
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-06-10 19:51:21 -0300

    Add underline to icon

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,19 +2,20 @@ import { ExternalLink } from 'lucide-react';
 
 export function Footer() {
    return (
-      <footer className='flex items-center justify-center gap-1 text-sm p-6 border-t border-green-500'>
-         <p>
-            <span className='text-green-100'>Desenvolvido por </span>
-            <a
-               href='https://github.com/philipeoliveira'
-               title='Abrir em nova aba o GitHub do autor Philipe Oliveira'
-               target='_blank'
-               className='text-green-100 border-b hover:text-green-300 hover:border-b-0'
-            >
-               Philipe Oliveira
-            </a>
+      <footer className='p-6 border-t border-green-500'>
+         <p className='flex items-center justify-center gap-1 h-6 text-green-100 text-sm'>
+            Desenvolvido por
+            <span className='flex items-center gap-1 border-b hover:border-b-0 hover:text-green-300'>
+               <a
+                  href='https://github.com/philipeoliveira'
+                  title='Abrir em nova aba o GitHub do autor Philipe Oliveira'
+                  target='_blank'
+               >
+                  Philipe Oliveira
+               </a>
+               <ExternalLink size={14} />
+            </span>
          </p>
-         <ExternalLink size={14} />
       </footer>
    );
 }
