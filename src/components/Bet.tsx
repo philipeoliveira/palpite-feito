8f6376d120c8756d53464e4eeccd299dea1380a6
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-06-10 20:24:11 -0300

    Add Toast in Bet component

--- src/components/Bet.tsx
+++ src/components/Bet.tsx
@@ -1,6 +1,7 @@
-import { useState, useEffect, useContext } from 'react';
+import * as Toast from '@radix-ui/react-toast';
+import { useState, useEffect, useContext, useRef } from 'react';
 import { twMerge } from 'tailwind-merge';
-import { RotateCw } from 'lucide-react';
+import { Info, RotateCw } from 'lucide-react';
 
 import { Subtitle } from './Subtitle';
 import { BetInfoCard } from './BetInfoCard';
@@ -23,14 +24,19 @@ export function Bet() {
 
    const [selectedNumbers, setSelectedNumbers] = useState<string[]>([]);
 
-   function createBet(totalNumbersAvailable: string, minNumbersToBet: string) {
-      const generatedBet = generateBet(minNumbersToBet, totalNumbersAvailable);
-      setSelectedNumbers(generatedBet);
-   }
+   const [open, setOpen] = useState(false);
+   const timerRef = useRef(0);
 
+   function handleToastCustom() {
+      setOpen(false);
+      window.clearTimeout(timerRef.current);
+      timerRef.current = window.setTimeout(() => {
+         setOpen(true);
+      }, 100);
+   }
    useEffect(() => {
-      createBet(totalNumbersAvailable, minNumbersToBet);
-   }, [totalNumbersAvailable, minNumbersToBet]);
+      return () => clearTimeout(timerRef.current);
+   }, []);
 
    function handleSelectedNumber(e: React.ChangeEvent<HTMLInputElement>) {
       const selectedNumber = e.target.name;
@@ -38,13 +44,21 @@ export function Bet() {
       if (e.target.checked) {
          selectedNumbers.length < +maxNumbersToBet
             ? setSelectedNumbers((prevState) => [...prevState, selectedNumber])
-            : alert('O limite de números para esta aposta foi atingido.');
+            : handleToastCustom();
       } else {
          setSelectedNumbers((prevState) => {
             return [...prevState.filter((numString) => numString !== selectedNumber)];
          });
       }
    }
+
+   function createBet(totalNumbersAvailable: string, minNumbersToBet: string) {
+      const generatedBet = generateBet(minNumbersToBet, totalNumbersAvailable);
+      setSelectedNumbers(generatedBet);
+   }
+   useEffect(() => {
+      createBet(totalNumbersAvailable, minNumbersToBet);
+   }, [totalNumbersAvailable, minNumbersToBet]);
 
    return (
       <section
@@ -62,31 +76,54 @@ export function Bet() {
             <div className='flex flex-col gap-10'>
                <form id='bet-form'>
                   <fieldset className='grid grid-cols-10 gap-3'>
-                     {generateAvailableNumbersForBet(totalNumbersAvailable).map(
-                        (numString) => (
-                           <label
-                              key={numString}
-                              className={twMerge(
-                                 'flex items-center justify-center text-3xl w-[50px] h-[50px] border-2 rounded-full cursor-pointer duration-100 hover:scale-110 hover:duration-100',
-                                 selectedNumbers.includes(numString)
-                                    ? 'text-gray-900 font-medium bg-green-300 border-green-300'
-                                    : 'text-green-500 bg-gray-900 border-green-500'
-                              )}
-                           >
-                              <input
-                                 type='checkbox'
-                                 name={`${numString}`}
-                                 id={`number-${numString}`}
-                                 onChange={handleSelectedNumber}
-                                 checked={
-                                    selectedNumbers.includes(numString) ? true : false
-                                 }
-                                 className='hidden'
-                              />
-                              {numString}
-                           </label>
-                        )
-                     )}
+                     <Toast.Provider swipeDirection='up'>
+                        {generateAvailableNumbersForBet(totalNumbersAvailable).map(
+                           (numString) => (
+                              <label
+                                 key={numString}
+                                 className={twMerge(
+                                    'flex items-center justify-center text-3xl w-[50px] h-[50px] border-2 rounded-full cursor-pointer duration-100 hover:scale-110 hover:duration-100',
+                                    selectedNumbers.includes(numString)
+                                       ? 'text-gray-900 font-medium bg-green-300 border-green-300'
+                                       : 'text-green-500 bg-gray-900 border-green-500'
+                                 )}
+                              >
+                                 <input
+                                    type='checkbox'
+                                    name={`${numString}`}
+                                    id={`number-${numString}`}
+                                    onChange={handleSelectedNumber}
+                                    checked={
+                                       selectedNumbers.includes(numString) ? true : false
+                                    }
+                                    className='hidden'
+                                 />
+                                 {numString}
+                              </label>
+                           )
+                        )}
+                        <Toast.Root
+                           open={open}
+                           onOpenChange={setOpen}
+                           duration={3000}
+                           className={twMerge(
+                              'bg-green-100 rounded shadow-lg p-3',
+                              'toast-root'
+                           )}
+                        >
+                           <Toast.Description asChild>
+                              <div className='flex items-center justify-center gap-2 text-gray-900 font-medium bg-green-100'>
+                                 <Info
+                                    size={20}
+                                    strokeWidth={2}
+                                    className='text-green-100 bg-gray-900 rounded-full'
+                                 />
+                                 O limite de números para este palpite foi atingido
+                              </div>
+                           </Toast.Description>
+                        </Toast.Root>
+                        <Toast.Viewport className='max-w-lg p-6 fixed bottom-0 left-1/2 -translate-x-1/2 z-50' />
+                     </Toast.Provider>
                   </fieldset>
                </form>
 
