3649281a0f9219ac8f6e64df4685805435cc064c
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-06-12 19:20:36 -0300

    Modify MenuButton styles

--- src/components/MenuButton.tsx
+++ src/components/MenuButton.tsx
@@ -8,7 +8,7 @@ export function MenuButton(props: MenuButtonProps) {
    return (
       <button
          {...props}
-         className='flex items-center justify-center gap-2 text-sm text-green-300  uppercase py-1 px-5 border border-green-700 rounded duration-200 hover:border-green-300 hover:-translate-y-[2px] hover:duration-200'
+         className='flex items-center justify-center gap-2 text-sm leading-6 uppercase text-green-300 py-1 px-5 bg-gray-900 border border-green-700 rounded duration-200 hover:border-green-300 hover:-translate-y-[2px] hover:duration-200'
       >
          {props.children}
       </button>
