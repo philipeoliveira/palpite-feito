5fc50229d90659636dfaa9b507292262cc2d7894
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-06-17 19:36:50 -0300

    Adjust button focus styles

--- src/components/ModalitiesButtons.tsx
+++ src/components/ModalitiesButtons.tsx
@@ -32,7 +32,7 @@ export function ModalitiesButtons() {
          key={modality.id}
          htmlFor={lowercaseWithoutAccents(modality.name)}
          className={twMerge(
-            'has-[:focus]:border-green-300 text-xl text-green-300 border border-green-700 py-5 px-8 rounded duration-200',
+            'text-xl text-green-300 border border-green-700 py-5 px-8 rounded duration-200 has-[:focus]:border-green-300',
             selectedModality.name === modality.name
                ? '-translate-y-[2px] border-b-green-300'
                : 'hover:-translate-y-[2px] hover:border-green-300 hover:duration-200 cursor-pointer'
@@ -45,7 +45,7 @@ export function ModalitiesButtons() {
             aria-label={`Aba da ${modality.name}`}
             value={modality.name}
             onChange={handleSelectedModality}
-            className='w-0 h-0 opacity-0 focus:border-none'
+            className='w-0 h-0 opacity-0'
          />
          {`${modality.name}`}
       </label>
