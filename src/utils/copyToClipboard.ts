fecb0f35b256764991b7c07a4ccbe08cf63a4520
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-06-22 18:32:45 -0300

    Refactor function copyToClipboard to receive string too

--- src/utils/copyToClipboard.ts
+++ src/utils/copyToClipboard.ts
@@ -1,20 +1,29 @@
+import sortNumbersAscending from './sortNumbersAscending';
+
 /**
  * @description Copia texto para área de transferência e retorna aviso pelo Toast.
- * @param {string[]} selectedNumbers Array de string -> Array com números selecionados para a cópia.
+ * @param {string[] | string} targetToCopy Array de string ou string -> Conteúdo para ser copiado.
  * @param {function():void} toast Function -> Função que lida com o Toast de algum conteúdo.
  * @example
- * // Se a sequência para o palpite for copiada: 20, 27, 30, 37, 50, 58
+ * // Se o conteúdo para ser copiado for: 20, 27, 30, 37, 50, 58
  * // Resultado esperado: 20-27-30-37-50-58
  */
 
-import sortNumbersAscending from './sortNumbersAscending';
+export const copyToClipboard = async (
+   targetToCopy: string[] | string,
+   toast: () => void
+) => {
+   let target;
 
-export const copyToClipboard = async (selectedNumbers: string[], toast: () => void) => {
-   const numbersAscending = sortNumbersAscending(selectedNumbers);
-   const formattedNumbersAscending = numbersAscending.join('-');
+   if (typeof targetToCopy === 'object') {
+      const numbersAscending = sortNumbersAscending(targetToCopy);
+      target = numbersAscending.join('-');
+   } else {
+      target = targetToCopy;
+   }
 
    try {
-      await navigator.clipboard.writeText(formattedNumbersAscending);
+      await navigator.clipboard.writeText(target);
       toast();
    } catch (err) {
       console.error('Falha ao copiar texto', err);
