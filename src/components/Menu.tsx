d9009879db56b8a5c876256d9e4b9cbd21637ac4
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-07-11 16:26:50 -0300

    Add animation to the responsive menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 import { AppWindow, TicketPlus, MenuIcon, X } from 'lucide-react';
 import { Button } from './Button';
 
@@ -38,30 +39,36 @@ export function Menu() {
          </Button>
 
          {/* Menu Mobile */}
-         <div
-            className={`fixed top-0 left-0 w-full h-full bg-gray-900 z-10 ${
-               isShowMenu ? 'block' : 'hidden'
-            }`}
-         >
-            <ul className='flex flex-col items-center w-full gap-4 p-4 mt-14'>
-               <li>
-                  <Link to='/' tabIndex={-1}>
-                     <Button onClick={handleToggleMenu}>
-                        <TicketPlus className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
-                        Criar palpite
-                     </Button>
-                  </Link>
-               </li>
-               <li>
-                  <Link to='/about' tabIndex={-1}>
-                     <Button onClick={handleToggleMenu}>
-                        <AppWindow className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
-                        Sobre o site
-                     </Button>
-                  </Link>
-               </li>
-            </ul>
-         </div>
+         {isShowMenu && (
+            <motion.div
+               initial={{ y: '-100vh' }}
+               animate={{ y: 0 }}
+               transition={{ duration: 0.3, ease: 'anticipate' }}
+               className={`fixed top-0 left-0 w-full h-full bg-gray-900 z-10`}
+            >
+               <ul
+                  id='mobile-menu'
+                  className='flex flex-col items-center w-full gap-4 p-4 mt-14'
+               >
+                  <li>
+                     <Link to='/' tabIndex={-1}>
+                        <Button onClick={handleToggleMenu}>
+                           <TicketPlus className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
+                           Criar palpite
+                        </Button>
+                     </Link>
+                  </li>
+                  <li>
+                     <Link to='/about' tabIndex={-1}>
+                        <Button onClick={handleToggleMenu}>
+                           <AppWindow className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
+                           Sobre o site
+                        </Button>
+                     </Link>
+                  </li>
+               </ul>
+            </motion.div>
+         )}
       </nav>
    );
 }
