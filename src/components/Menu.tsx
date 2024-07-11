17e5166e329e061b6ab8465f8f398eca3bee4d08
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-07-11 16:49:33 -0300

    Add accessibility tags to the responsive menu

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -12,20 +12,24 @@ export function Menu() {
    }
 
    return (
-      <nav className='flex justify-between items-center'>
+      <nav
+         className='flex justify-between items-center'
+         aria-label='Menu Principal'
+         role='navigation'
+      >
          {/* Menu para Desktop */}
-         <ul className='hidden md:flex gap-2 md:gap-3 lg:gap-5'>
-            <li>
+         <ul className='hidden md:flex gap-2 md:gap-3 lg:gap-5' role='menubar'>
+            <li role='menuitem'>
                <Link to='/' tabIndex={-1}>
-                  <Button>
+                  <Button aria-label='Link para página inicial de palpite'>
                      <TicketPlus size={16} />
                      <span className='hidden md:block lg:block'>Criar palpite</span>
                   </Button>
                </Link>
             </li>
-            <li>
+            <li role='menuitem'>
                <Link to='/about' tabIndex={-1}>
-                  <Button>
+                  <Button aria-label='Link para página sobre o site'>
                      <AppWindow size={16} />
                      <span className='hidden md:block lg:block'>Sobre o site</span>
                   </Button>
@@ -34,7 +38,13 @@ export function Menu() {
          </ul>
 
          {/* Ícone de Menu para Mobile */}
-         <Button className='md:hidden z-20' onClick={handleToggleMenu}>
+         <Button
+            className='md:hidden z-20'
+            onClick={handleToggleMenu}
+            aria-label={isShowMenu ? 'Fechar Menu' : 'Abrir Menu'}
+            aria-expanded={isShowMenu}
+            aria-haspopup='true'
+         >
             {isShowMenu ? <X size={16} /> : <MenuIcon size={16} />}
          </Button>
 
@@ -47,20 +57,26 @@ export function Menu() {
                className={`fixed top-0 left-0 w-full h-full bg-gray-900 z-10`}
             >
                <ul
-                  id='mobile-menu'
+                  role='menubar'
                   className='flex flex-col items-center w-full gap-4 p-4 mt-14'
                >
-                  <li>
+                  <li role='menuitem'>
                      <Link to='/' tabIndex={-1}>
-                        <Button onClick={handleToggleMenu}>
+                        <Button
+                           onClick={handleToggleMenu}
+                           aria-label='Link para página inicial de palpite'
+                        >
                            <TicketPlus className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
                            Criar palpite
                         </Button>
                      </Link>
                   </li>
-                  <li>
+                  <li role='menuitem'>
                      <Link to='/about' tabIndex={-1}>
-                        <Button onClick={handleToggleMenu}>
+                        <Button
+                           onClick={handleToggleMenu}
+                           aria-label='Link para página sobre o site'
+                        >
                            <AppWindow className='w-2.5 md:w-4 lg:w-4 h-2.5 md:h-4 lg:h-4' />
                            Sobre o site
                         </Button>
